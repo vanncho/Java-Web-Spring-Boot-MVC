b51b891a6e2297a99b078ed37d6ff88114a13983
Ivan Ivanov <van_cho@abv.bg>
2017-10-25 15:57:02 +0300

    Fix null of "totalCost" input on every page.

--- online-bus-reservation/src/main/resources/static/scripts/booking.js
+++ online-bus-reservation/src/main/resources/static/scripts/booking.js
@@ -1,41 +1,46 @@
 // SUM TOTAL COST OF RESERVED SEATS BY TICKET PRICE
 
-let price = document.getElementById('totalCost').value = 0;
+let price = document.getElementById('totalCost');
 
-function calc(checkbox) {
+if (price !== null) {
+	
+	price.value = 0;
+	
+	function calc(checkbox) {
 
-    if (checkbox.checked) {
+	    if (checkbox.checked) {
 
-        let a = document.getElementById('ticketPrice').value;
-        let intA = parseFloat(a);
+	        let a = document.getElementById('ticketPrice').value;
+	        let intA = parseFloat(a);
 
-        let b = document.getElementById('totalCost').value;
-        let intB = parseFloat(b);
+	        let b = document.getElementById('totalCost').value;
+	        let intB = parseFloat(b);
 
-        document.getElementById('totalCost').value = (intA + intB).toFixed(2);
-    } else {
+	        document.getElementById('totalCost').value = (intA + intB).toFixed(2);
+	    } else {
 
-        let a = document.getElementById('totalCost').value;
-        let intA = parseFloat(a);
+	        let a = document.getElementById('totalCost').value;
+	        let intA = parseFloat(a);
 
-        let b = document.getElementById('ticketPrice').value;
-        let intB = parseFloat(b);
+	        let b = document.getElementById('ticketPrice').value;
+	        let intB = parseFloat(b);
 
-        document.getElementById('totalCost').value = (intA - intB).toFixed(2);
-    }
-};
+	        document.getElementById('totalCost').value = (intA - intB).toFixed(2);
+	    }
+	};
 
-function checkDate() {
+	function checkDate() {
 
-    let selectedText = document.getElementById('datepicker').value;
-    let selectedDate = new Date(selectedText);
-    let now = new Date();
+	    let selectedText = document.getElementById('datepicker').value;
+	    let selectedDate = new Date(selectedText);
+	    let now = new Date();
 
-    // if (selectedText == "" || selectedText == null) {
-    //     document.getElementById('datepicker').setAttribute("style", "background-color: red")
-    // }
+	    // if (selectedText == "" || selectedText == null) {
+	    //     document.getElementById('datepicker').setAttribute("style", "background-color: red")
+	    // }
 
-    if (selectedDate <= now) {
-        document.getElementById('datepicker').setAttribute("style", "background-color: red")
-    }
-};
+	    if (selectedDate <= now) {
+	        document.getElementById('datepicker').setAttribute("style", "background-color: red")
+	    }
+	};
+}
